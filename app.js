d46861efeaadfd9a25080d2b8796e2d1fcdc8a66
EeeasyCode <eeeasycode@gmail.com>
2023-11-13 15:08:28 +0900

    [FEAT] 개발 환경 세팅

--- app.js
+++ app.js
@@ -18,14 +18,17 @@ nunjucks.configure("views", {
 });
 
 sequelize.sync({ force: false})
-.then(() => {
-    console.log("DB connect success");
-})
+    .then(() => {
+        console.log("DB connect success");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 
 app.listen(app.get("port"), () => {
