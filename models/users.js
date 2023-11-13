969df1d26c3e6b0996e0adcdb21a985990e8a458
EeeasyCode <eeeasycode@gmail.com>
2023-11-13 15:09:15 +0900

    [FEAT] create users model

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            name: {
+                type: Sequelize.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+        })
+    }
+}
