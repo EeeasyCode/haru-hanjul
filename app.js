32b13239ada4bf86a9e5b687016fa309155cc1d4
EeeasyCode <eeeasycode@gmail.com>
2023-11-13 12:22:09 +0900

    [FEAT] 개발 환경 세팅

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
 const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
 
 dotenv.config();
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () => {
