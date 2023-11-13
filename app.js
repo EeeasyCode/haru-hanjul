cbd189b2246fb8428bfe91f23cea9cb04db2c125
EeeasyCode <eeeasycode@gmail.com>
2023-11-13 15:05:31 +0900

    [FEAT] 개발 환경 세팅

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const nunjucks = require("nunjucks");
+
+const { sequelize } = require("./models");
+
+const app = express();
 
 dotenv.config();
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+    express: app,
+    watch: true,
+});
+
+sequelize.sync({ force: false})
+.then(() => {
+    console.log("DB connect success");
+})
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.set("port", process.env.PORT || 3000);
+
 
 app.listen(app.get("port"), () => {
     console.log(`${app.get("port")} port is running ..`);
