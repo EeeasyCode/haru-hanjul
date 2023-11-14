c5294ce0316f28ecae49160c10a97804559f2393
EeeasyCode <eeeasycode@gmail.com>
2023-11-15 00:37:02 +0900

    [FEAT] 중복 처리 구현

--- repository/users.repository.js
+++ repository/users.repository.js
@@ -1,7 +1,10 @@
 const { Users } = require('../models');
 
 class UserRepository {
-
+  findUser = async (username) => {
+    const findUserData = await Users.findOne({ where: username });
+    return findUserData;
+  }
   createUser = async (username, password) => {
     
     const createUserData = await Users.create({ username, password });
--- services/users.service.js
+++ services/users.service.js
@@ -5,8 +5,12 @@ class UserService {
   userRepository = new UserRepository();
 
   createUser = async (username, password) => {
+    const checkUser = await this.userRepository.findUser({ username });
+    if (checkUser) {
+      return "이미 존재하는 계정입니다."
+    }
     const hashPassword = await bcrypt.hash(password, 10);
-    const createUserData = await this.userRepository.createUser(username, hashPassword);
+    await this.userRepository.createUser(username, hashPassword);
 
     return "success create user";
   }
