e0b181791840838c0564b9914ce4af8a2215547e
EeeasyCode <eeeasycode@gmail.com>
2023-11-15 10:58:38 +0900

    [FEAT] JWT 발급 기능 구현

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,5 +1,10 @@
-const UserRepository = require("../repository/users.repository");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+
+const UserRepository = require("../repository/users.repository");
+
+dotenv.config();
 
 class AuthService {
   userRepository = new UserRepository();
@@ -11,8 +16,27 @@ class AuthService {
     if (!checkUser || hashPassword === false) {
         return "check id or pw"
     }
+    
+    const authToken = this.createToken(username);
+    
+    return authToken
+  }
 
-    return "success login"
+  createToken = async (username) => {
+    const key = process.env.SECRET_KEY;
+
+    const token = jwt.sign(
+      {
+        type: "JWT",
+        username: username,
+      },
+      key,
+      {
+        expiresIn: "1m", // 15분후 만료
+        issuer: "admin",
+      }
+    );
+    return token
   }
 }
 
