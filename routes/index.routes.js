4a4b10836b8c1e01335e7a9756fa071c2eb7821c
EeeasyCode <eeeasycode@gmail.com>
2023-11-18 22:32:37 +0900

    [FEAT] jwt token 401 error 파악

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { verifyToken } = require('../middlewares/verifyToken');
 const router = express.Router();
 const passport = require("passport");
 
@@ -7,9 +6,14 @@ router.get("/", (req, res) => {
     res.render("entry_page");
 });
 
-router.use('/main', passport.authenticate('jwt', { session: false }));
-router.get("/main", (req, res) => {
-    res.render("main");
+router.get("/main", passport.authenticate('jwt', { session: false }), (req, res) => {
+    try {
+        console.log(req)
+        return res.render('main')
+    } catch (error) {
+        console.log(req)
+        return res.redirect("/?error=TokenExpiredError");
+    }
 });
 
  
