42618c84fb6838e63a0e319a3e04bf7314f8c6d2
EeeasyCode <eeeasycode@gmail.com>
2023-11-18 15:38:50 +0900

    [FEAT] auth service login 로직 수정

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
+const passport = require("passport");
 
 const UserRepository = require("../repository/users.repository");
 
@@ -10,39 +11,73 @@ class AuthService {
   userRepository = new UserRepository();
 
   authLogin = async (email, password) => {
-    const checkUser = await this.userRepository.findUser({ email });
+    try {
+      passport.authenticate('local', user, (req, res, next) => {
+        if(passportError || !user) {
+          res.status(400).json({
+            message: info.reason
+          });
+          return false
+        }
+
+        req.login(user, {session: false}, (loginError) => {
+          if (loginError) {
+            res.send(loginError);
+            return false
+          }
+          const key = process.env.SECRET_KEY;
+          const token = jwt.sign(
+            {
+              type: "JWT",
+              email: email,
+            },
+            key,
+            {
+              expiresIn: "10m", 
+              issuer: "admin",
+            }
+          );
+          return token;
+        });
+      });
+    } catch(error) {
+      console.error(error);
+      next(error);
+    };
+  };
+  // const checkUser = await this.userRepository.findUser({ email });
     
-    if (checkUser === null) {
-      return false
-    }
+  //   if (checkUser === null) {
+  //     return false
+  //   }
 
-    const hashPassword = await bcrypt.compare(password, checkUser.password);
+  //   const hashPassword = await bcrypt.compare(password, checkUser.password);
 
-    if (hashPassword === false) {
-        return false
-    }
+  //   if (hashPassword === false) {
+  //       return false
+  //   }
 
-    const authToken = this.createToken(email);
+  //   const authToken = this.createToken(email);
 
-    return authToken
-  }
+  //   return authToken
+  // }
 
-  createToken = async (email) => {
-    const key = process.env.SECRET_KEY;
+  // createToken = async (email) => {
+  //   const key = process.env.SECRET_KEY;
 
-    const token = jwt.sign(
-      {
-        type: "JWT",
-        email: email,
-      },
-      key,
-      {
-        expiresIn: "1m", 
-        issuer: "admin",
-      }
-    );
-    return token
-  }
+  //   const token = jwt.sign(
+  //     {
+  //       type: "JWT",
+  //       email: email,
+  //     },
+  //     key,
+  //     {
+  //       expiresIn: "1m", 
+  //       issuer: "admin",
+  //     }
+  //   );
+  //   return token
+  // }
 }
 
 module.exports = AuthService;
